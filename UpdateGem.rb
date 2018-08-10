a83feabf71c7687902f86e9958e85225dbec5fa7
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2018-08-10 14:06:22 -0500

    add UploadGem.rb script

--- UpdateGem.rb
+++ UpdateGem.rb
@@ -1,7 +1,6 @@
 require 'rest-client'
 
 # Get Gem API Key, write it into ~/.gem/credentials
-
 rubyGemUserName = ENV['RUBYGEMSUSERNAME']
 rubyGemPass = ENV['RUBYGEMSPASSWORD']
 
@@ -12,5 +11,38 @@ token = RestClient::Request.new(
   :password => rubyGemPass,
 ).execute
 
-Dir.mkdir Dir.home + '/.gem'
 File.open(Dir.home + '/.gem/credentials', 'w') { |file| file.write(token) }
+
+# Generate required files
+
+# Generate new version string
+oldVersion = %x( gem search voiceit2 )[/\((.*?)\)/, 1]
+oldVersionArray = oldVersion.split('.')
+oldVersionArray[2] = (oldVersionArray[2].to_i + 1).to_s
+newVersion = oldVersionArray.join('.')
+
+# Move ./VoiceIt2.rb to ./lib/VoiceIt2
+# Dir.mkdir './lib'
+# File.rename ./VoiceIt2.rb, ./lib/VoiceIt2.rb
+
+date = Time.now.strftime("%Y-%m-%d")
+
+# Generate Gemspec file
+gemspec = "Gem::Specification.new do |s|
+  s.name        = 'VoiceIt2'
+  s.version     = '" + newVersion + "'
+  s.date        = '" + date + "'
+  s.summary     = 'VoiceIt Api 2'
+  s.description = 'A wrapper for VoiceIt's API 2'
+  s.authors     = ['StephenAkers']
+  s.email       = 'stephen@voiceit.io'
+  s.files       = ['./VoiceIt2.rb']
+  s.homepage    =
+    'http://rubygems.org/gems/hola'
+  s.license       = 'MIT'
+end"
+
+File.open(Dir.home + './VoiceIt2.gemspec', 'w') { |file| file.write(gemspec) }
+
+system 'gem build VoiceIt2.gemspec'
+system 'gem push VoiceIt2' + newVersion + '.gem'
