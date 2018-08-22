ef2358ce07c0f1ca474b59f8237eb35c2f617994
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2018-08-22 16:53:19 -0500

    update UpdateGem.rb. (test failed REALEASE)

--- UpdateGem.rb
+++ UpdateGem.rb
@@ -3,15 +3,30 @@ require 'rest-client'
 # Generate required files
 
 # Generate new version string
+oldVersion = %x( gem search voiceit2 )[/\((.*?)\)/, 1]
+oldVersionArray = oldVersion.split('.')
 commit = %x( git log -1 --pretty=%B | head -n 1 )
-if commit.include? "Release"
-  oldVersion = %x( gem search voiceit2 )[/\((.*?)\)/, 1]
-  oldVersionArray = oldVersion.split('.')
-  oldVersionArray[2] = (oldVersionArray[2].to_i + 1).to_s
+if commit.include? "RELEASE"
+
+
+  system 'gem build VoiceIt2.gemspec'
+  system 'gem push VoiceIt2-' + newVersion + '.gem'
+  if commit.include? "RELEASEMAJOR"
+    oldVersionArray[0] = (oldVersionArray[0].to_i + 1).to_s
+    oldVersionArray[1] = '0'
+    oldVersionArray[2] = '0'
+  elsif commit.include? "RELEASEMINOR"
+    oldVersionArray[1] = (oldVersionArray[1].to_i + 1).to_s
+    oldVersionArray[2] = '0'
+  elsif commit.include? "RELEASEPATCH"
+    oldVersionArray[2] = (oldVersionArray[2].to_i + 1).to_s
+  else
+    puts "Must specify RELEASEMAJOR, RELEASEMINOR, or RELEASEPATCH in the title."
+    exit(1)
+  end
+
   newVersion = oldVersionArray.join('.')
-
   date = Time.now.strftime("%Y-%m-%d")
-
   # Generate Gemspec file
   gemspec = "Gem::Specification.new do |s|
     s.name        = 'VoiceIt2'
@@ -26,9 +41,5 @@ if commit.include? "Release"
       'http://rubygems.org/gems/hola'
     s.license       = 'MIT'
   end"
-
   File.open('./VoiceIt2.gemspec', 'w') { |file| file.write(gemspec) }
-
-  system 'gem build VoiceIt2.gemspec'
-  system 'gem push VoiceIt2-' + newVersion + '.gem'
 end
